95b46ed631962f71052e7a94a539852fe8cdf5e4
RubyXiong00 <xiongruby00@gmail.com>
2024-12-27 13:04:40 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -193,6 +193,7 @@ def main(args):
         os.system(cmd)
 
         myutils.gen_360video(tmp_ambix_fn, tmp_video_fn, args.output_fn, overlay_map=args.overlay_map, inject_meta=args.VR, binauralize=not args.VR)
+        # 生成360视频，包括声源位置掩膜层
 
         os.remove(tmp_video_fn)
     os.remove(tmp_ambix_fn)
